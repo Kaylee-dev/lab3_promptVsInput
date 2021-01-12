72493bb95c972d4daec006fc74f7b32ece461628
Kaylee <99057337@mydavinci.nl>
2021-01-12 12:23:55 +0100

    toegevoegd aan repository

---
+++ promptVsInput.js
@@ -0,0 +1,10 @@
+var question = prompt("Voer een getal in");
+var numbers = [];
+
+for(var i = 0; i <= question; i++){
+	document.getElementById("text");
+	numbers.push(i);
+	text.innerHTML = "<p>array:<p/>" + numbers;
+	document.write(numbers + "<br/>");
+	console.log(numbers);
+}
