b6094f007770f7ba5070c3e318132de4ef0c7a7c
Kaylee <99057337@mydavinci.nl>
2021-01-12 12:30:50 +0100

    comment toegevoegd

--- promptVsInput.js
+++ promptVsInput.js
@@ -2,9 +2,10 @@ var question = prompt("Voer een getal in");
 var numbers = [];
 
 for(var i = 0; i <= question; i++){
-	document.getElementById("text");
-	numbers.push(i);
+	document.getElementById("text");// Hier haal ik de p op van mijn HTML.
+	numbers.push(i);// Hier push ik de var i in de array, dus wat ingevoerd word bij mijn prompt genaamd question zet die in de array.
 	text.innerHTML = "<p>array:<p/>" + numbers;
-	document.write(numbers + "<br/>");
+	document.write(numbers + "<br/>");	
 	console.log(numbers);
 }
+
